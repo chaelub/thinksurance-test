2ab713ffa27411a1098e84ffa876ccc82c293060
Vadim Kravchenko <v.kravchenko@s-e.su>
2020-11-08 20:59:02 +0300

    main -> pig-Latin - split tests to separate files;

--- pig-latin/pig-latin_test.go
+++ pig-latin/pig-latin_test.go
@@ -5,47 +5,6 @@ import (
 	"testing"
 )
 
-var invalidInputsWiki = []string{
-	"",
-	"ay",
-	"say",
-	"day",
-	"hello",
-	"helloay",
-}
-
-func TestInvalidInputWiki(t *testing.T) {
-	for i := range invalidInputsWiki {
-		isPigLatin := isPigLatinWiki(invalidInputsWiki[i])
-		if isPigLatin {
-			t.Error(fmt.Sprintf("the word [%s] is a pig Latin word\n", invalidInputsWiki[i]))
-		}
-	}
-}
-
-var validInputsWiki = []string{
-	"iay",
-	"aay",
-	"ellohay",
-	"allyay",
-}
-
-func TestValidInputWiki(t *testing.T) {
-	for i := range validInputsWiki {
-		isPigLatin := isPigLatinWiki(validInputsWiki[i])
-		if !isPigLatin {
-			t.Error(fmt.Sprintf("the word [%s] is a pig Latin word\n", validInputsWiki[i]))
-		}
-	}
-}
-
-func TestCaseWiki(t *testing.T) {
-	if (isPigLatinWiki("iya") != isPigLatinWiki("Iya")) ||
-		(isPigLatinWiki("i") != isPigLatinWiki("I")) {
-		t.Error("the case of letters shouldn't affect the test result")
-	}
-}
-
 var validInputs = [][2]string{
 	{"tomorrow", "morrowto"},
 	{"hello", "llohe"},
