7a84adb2445ec88690ed77d4adb713ebbdd95dd5
Vadim Kravchenko <v.kravchenko@s-e.su>
2020-11-08 20:53:01 +0300

    main -> pig-Latin - new implementation: check one word is a candidate for a pig latin of another word;

--- pig-latin/pig-latin.go
+++ pig-latin/pig-latin.go
@@ -1,13 +1,14 @@
 package pig_latin
 
 import (
+	"bytes"
 	"strings"
 )
 
 const (
 	validOneLetterWords = "ai"
-	vowels              = "aeiuo"
-	suffix              = "ya"
+	vowelsStr           = "aeiuo"
+	suffix              = "ay"
 )
 
 var (
@@ -16,17 +17,17 @@ var (
 )
 
 /*
-
-
- */
-func isPigLatin(word string) bool {
+the isPigLatinWiki algorithm what follows wiki rules
+https://en.wikipedia.org/wiki/Pig_Latin
+*/
+func isPigLatinWiki(word string) bool {
 	word = strings.ToLower(word)
 
-	// any pig Latin word has to be >= 3 digits (a - > aya, I -> Iya)
+	// any pig Latin word has to be >= 3 digits (a - > aay, I -> Iay)
 	if len(word) < 3 {
 		return false
 	}
-	// a pig Latin word has to contain suffix 'ya'. If it doesn't, it's not a pig Latin word
+	// a pig Latin word has to contain suffix 'ay'. If it doesn't, it's not a pig Latin word
 	if !strings.HasSuffix(word, suffix) {
 		return false
 	}
@@ -36,8 +37,8 @@ func isPigLatin(word string) bool {
 	}
 	// it's not a pig Latin word if the word starts with consonant, but it's a vowel right before the suffix
 	wordLen := len(word)
-	if !strings.ContainsAny(word[:1], vowels) &&
-		strings.ContainsAny(word[wordLen-testLetterOffset:wordLen-suffixLen], vowels) {
+	if !strings.ContainsAny(word[:1], vowelsStr) &&
+		strings.ContainsAny(word[wordLen-testLetterOffset:wordLen-suffixLen], vowelsStr) {
 		return false
 	}
 
@@ -45,11 +46,58 @@ func isPigLatin(word string) bool {
 }
 
 /*
-maybe I should solve this task as "is a string a permutation of another string",
-because of a strange example in the task description:
-	"You have 2 given words as pig Latin candidates (e.g. center → tercen, tomorrow → morrowto).
-     Your task is to write a method returning true/false if the given word is a pig Latin or not."
+an implementation of isPigLatin algorithm without the suffix 'ay' ->
+we have an original word and the pig Latin candidate
 */
-func isPermutation(init, mutated string) bool {
-	return false
+func isPigLatin(origin, candidate string) bool {
+	origin = strings.ToLower(origin)
+	candidate = strings.ToLower(candidate)
+
+	if len(origin) != len(candidate) {
+		return false
+	}
+	offset := 0
+	if !strings.ContainsAny(candidate[:1], vowelsStr) {
+		prefLen := getPrefixLen(origin)
+		if prefLen >= 0 {
+			offset = prefLen
+		}
+	}
+
+	originRunes := []rune(origin)
+	candidateRunes := []rune(candidate)
+
+	for i := offset; i < len(originRunes); i++ {
+		if originRunes[i] != candidateRunes[i-offset] {
+			return false
+		}
+	}
+
+	for i := 0; i < offset; i++ {
+		if originRunes[i] != candidateRunes[len(candidateRunes)-offset+i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+var (
+	vowels = []byte{'a', 'e', 'i', 'u', 'o'}
+)
+
+func getPrefixLen(word string) int {
+	foundVowel := false
+	for i, r := range word {
+		if !foundVowel && bytes.ContainsRune(vowels, r) {
+			foundVowel = true
+			continue
+		}
+		if foundVowel && !bytes.ContainsRune(vowels, r) {
+			// found a consonant
+			return i
+		}
+	}
+
+	return -1
 }
--- pig-latin/pig-latin_test.go
+++ pig-latin/pig-latin_test.go
@@ -5,43 +5,85 @@ import (
 	"testing"
 )
 
-var invalidInputs = []string{
+var invalidInputsWiki = []string{
 	"",
-	"ya",
-	"sya",
+	"ay",
+	"say",
 	"day",
 	"hello",
-	"helloya",
+	"helloay",
 }
 
-func TestInvalidInput(t *testing.T) {
-	for i := range invalidInputs {
-		isPigLatin := isPigLatin(invalidInputs[i])
+func TestInvalidInputWiki(t *testing.T) {
+	for i := range invalidInputsWiki {
+		isPigLatin := isPigLatinWiki(invalidInputsWiki[i])
 		if isPigLatin {
-			t.Error(fmt.Sprintf("the word [%s] is a pig Latin word\n", invalidInputs[i]))
+			t.Error(fmt.Sprintf("the word [%s] is a pig Latin word\n", invalidInputsWiki[i]))
 		}
 	}
 }
 
-var validInputs = []string{
-	"iya",
-	"aya",
-	"ellohya",
-	"allyya",
+var validInputsWiki = []string{
+	"iay",
+	"aay",
+	"ellohay",
+	"allyay",
+}
+
+func TestValidInputWiki(t *testing.T) {
+	for i := range validInputsWiki {
+		isPigLatin := isPigLatinWiki(validInputsWiki[i])
+		if !isPigLatin {
+			t.Error(fmt.Sprintf("the word [%s] is a pig Latin word\n", validInputsWiki[i]))
+		}
+	}
+}
+
+func TestCaseWiki(t *testing.T) {
+	if (isPigLatinWiki("iya") != isPigLatinWiki("Iya")) ||
+		(isPigLatinWiki("i") != isPigLatinWiki("I")) {
+		t.Error("the case of letters shouldn't affect the test result")
+	}
+}
+
+var validInputs = [][2]string{
+	{"tomorrow", "morrowto"},
+	{"hello", "llohe"},
+	{"I", "I"},
+	{"are", "are"},
 }
 
 func TestValidInput(t *testing.T) {
 	for i := range validInputs {
-		isPigLatin := isPigLatin(validInputs[i])
+		isPigLatin := isPigLatin(validInputs[i][0], validInputs[i][1])
 		if !isPigLatin {
-			t.Error(fmt.Sprintf("the word [%s] is a pig Latin word\n", validInputs[i]))
+			t.Error(
+				fmt.Sprintf("the word [%s] is a pig Latin for [%s]\n",
+					invalidInputs[i][1],
+					invalidInputs[i][0],
+				),
+			)
 		}
 	}
 }
 
-func TestCase(t *testing.T) {
-	if (isPigLatin("iya") != isPigLatin("Iya")) ||
-		(isPigLatin("i") != isPigLatin("I")) {
-		t.Error("the case of letters shouldn't affect the test result")
+var invalidInputs = [][2]string{
+	{"tomorrow", "tomorrow"},
+	{"different", "lenght"},
+	{"hello", "elloh"},
+	{"world", "dlrow"},
+}
+
+func TestInvalidInput(t *testing.T) {
+	for i := range invalidInputs {
+		isPigLatin := isPigLatin(invalidInputs[i][0], invalidInputs[i][1])
+		if isPigLatin {
+			t.Error(
+				fmt.Sprintf("the word [%s] is not a pig Latin for [%s]\n",
+					invalidInputs[i][1],
+					invalidInputs[i][0],
+				),
+			)
+		}
 	}
 }
